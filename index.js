3cdf237b833a96cd437e7e63748b387300712d20
CH04-08\ITD <adam@newmediastaff.com>
2022-08-17 15:41:15 +0800

    added line in index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import readline from 'readline'
 
 let question  = 'Select an action p/P - park | u/U - unpark | m/M - view map | s/S - view Stats:'
 
-const questionLine = readline.createInterface({
+const r1 = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     prompt: question
 })
 
 questionLine.prompt();
+
+r1.on('line')
